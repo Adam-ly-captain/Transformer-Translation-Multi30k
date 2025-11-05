21409fb9f8d6459891d1dcaeb0d7b448fa5d2ab5
AdamShyly <shyly@stu.xmu.edu.cn>
2025-11-05 22:32:49 +0800

    more activation function

--- utils/activation.py
+++ utils/activation.py
@@ -9,5 +9,13 @@ def get_activation_module(activation_str):
         return nn.ReLU()
     elif activation_str == "gelu":
         return nn.GELU()
+    elif activation_str == "leakyrelu":
+        return nn.LeakyReLU()
+    elif activation_str == "tanh":
+        return nn.Tanh()
+    elif activation_str == "sigmoid":
+        return nn.Sigmoid()
+    elif activation_str == "elu":
+        return nn.ELU()
     else:
         raise ValueError(f"Unsupported activation function: {activation_str}")
